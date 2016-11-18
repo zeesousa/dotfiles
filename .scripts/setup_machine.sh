20352260c6d976324e43281064a154cb57ec7c56
José Sousa <josesousa9000@gmail.com>
2016-11-18 14:30:07 +0000

    Fix rvm GPG signature

--- .scripts/setup_machine.sh
+++ .scripts/setup_machine.sh
@@ -16,6 +16,7 @@ npm install yarn -g
 #rails and ruby
 cd
 sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
