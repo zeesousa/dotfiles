a22b95c5e2bf6e2b26de9a29f2265e35ea1022f6
José Sousa <josesousa9000@gmail.com>
2016-11-18 12:33:53 +0000

    Switch rbenv with rvm

--- .scripts/setup_machine.sh
+++ .scripts/setup_machine.sh
@@ -15,19 +15,13 @@ npm install yarn -g
 
 #rails and ruby
 cd
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH" 
-
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-
-rbenv install 2.3.1
-rbenv global 2.3.1
+sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 2.3.1
+rvm use 2.3.1 --default
+ruby -v
 
 gem install bundler
 gem install rails
--- .zshrc
+++ .zshrc
@@ -1,20 +1,26 @@
 export ZSH=/home/jose/.oh-my-zsh
 
+#Themes
 ZSH_THEME="pygmalion"
 
-plugins=(git rbenv bundler docker)
+#Plugins
+plugins=(git docker)
 
+#Oh my zsh
 source $ZSH/oh-my-zsh.sh
 
-#fix stupid npm shit
+#Use user-local global npm packages
 NPM_PACKAGES="${HOME}/.npm-packages"
-
 PATH="$NPM_PACKAGES/bin:$PATH"
-
 unset MANPATH
 export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+
+#Nice things for golang
 export GOPATH="$HOME/.go"
 
+#My aliases
 alias update_system="sudo apt-get update && sudo apt-get upgrade"
-alias py_activate="source env/bin/activate"
- 
+alias bi="bundler install"
+
+#RVM setup
+export PATH="$PATH:$HOME/.rvm/bin"
