8ce1a23072fa0f0c00035d361e2375d5943366ab
José Sousa <josesousa9000@gmail.com>
2016-12-23 18:26:19 +0000

    Fix error in geometry theme prefix

--- .scripts/install_geometry.sh
+++ .scripts/install_geometry.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/frmendes/geometry
 
 mkdir -p $HOME/.oh-my-zsh/custom/themes/
-mv geometry/geometry.zsh $HOME/.oh-my-zsh/custom/themes/geometry.zsh-theme
+
 mv geometry/lib $HOME/.oh-my-zsh/custom/themes/
 mv geometry/plugins $HOME/.oh-my-zsh/custom/themes/
 
