ddc282d19b72c34344989030ce7d627c00f144b1
José Sousa <josesousa9000@gmail.com>
2017-01-24 15:01:39 +0000

    Add libpq as dependency for rails

--- .scripts/setup_machine.sh
+++ .scripts/setup_machine.sh
@@ -21,7 +21,7 @@ yarn global add gulp
 
 #rails and ruby
 cd
-sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev libpq-dev
 command curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
