e3b047d10d3475f10ddcb74d5f02033eeed1275d
José Sousa <josesousa9000@gmail.com>
2017-08-11 01:46:49 +0100

    Fix ruby stuff

--- .scripts/setup/ruby.sh
+++ .scripts/setup/ruby.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-
-rbenv install 2.4.0
-rbenv global 2.4.0
+sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+source ~/.rvm/scripts/rvm
+rvm install 2.4.0
+rvm use 2.4.0 --default
 ruby -v
 
