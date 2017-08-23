524559dd1f397d81f9245c430cfcfb21a98f4f8f
José Sousa <josesousa9000@gmail.com>
2017-08-23 01:31:02 +0100

    Fix icons

--- .scripts/ubuntu_setup.sh
+++ .scripts/ubuntu_setup.sh
@@ -3,7 +3,8 @@ sudo apt-get install -y software-properties-common
 # pretty stuff
 sudo add-apt-repository -y ppa:noobslab/themes
 sudo add-apt-repository -y ppa:noobslab/icons
-sudo apt-get update && sudo apt-get install -y arc-theme arc-icons
+sudo add-apt-repository -y ppa:moka/daily
+sudo apt-get update && sudo apt-get install -y arc-theme arc-icons moka-icon-theme
 
 # spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
