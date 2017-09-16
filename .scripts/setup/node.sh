e20e92b3c7fbef045859c75de316b34a15a7a35c
José Sousa <josesousa9000@gmail.com>
2017-09-16 15:29:41 +0100

    Update node install script to use the now very usable NVM

--- .scripts/setup/node.sh
+++ .scripts/setup/node.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 #node
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-sudo apt-get install -y nodejs
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install node
+nvm use node
+nvm alias default node
 
 #yarn by facebook, a better alternative to npm
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-sudo apt-get update && sudo apt-get install -y yarn
+sudo apt-get update && sudo apt-get install --no-install-recommends -y yarn
 
-yarn global add bower
 yarn global add gulp
 yarn global add eslint
