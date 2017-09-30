bd7eea62b1bc870414af804a39eafbebc968ca7e
José Sousa <josesousa9000@gmail.com>
2017-09-30 15:37:12 +0100

    Reorganize setup scripts

---
+++ .scripts/setup/dev_system_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+~/.scripts/setup/dependencies/apt_packages.sh
+~/.scripts/setup/dependencies/node.sh
+~/.scripts/setup/dependencies/ruby.sh
+~/.scripts/setup/dependencies/go.sh
+~/.scripts/setup/dependencies/zsh.sh
+~/.scripts/setup/dependencies/geometry.sh
+~/.scripts/setup/dependencies/psql.sh
+~/.scripts/setup/dependencies/vscode_packages.sh
+~/.scripts/setup/dependencies/chrome_driver.sh
