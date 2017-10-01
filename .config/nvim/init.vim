9ac2eb9a4759dec42d481670157cdb3d3221dc59
José Sousa <josesousa9000@gmail.com>
2017-10-02 00:33:57 +0100

    Nvim

---
+++ .config/nvim/init.vim
@@ -0,0 +1,18 @@
+set nocompatible
+set tabstop=4       
+set shiftwidth=4
+set softtabstop=4
+set expandtab 
+filetype off
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin("~/.config/nvim/bundle")
+
+Plugin 'VundleVim/Vundle.vim'
+
+Plugin 'scrooloose/syntastic'
+Plugin 'justinmk/vim-syntax-extra'
+Plugin 'bling/vim-airline'
+
+call vundle#end()      
+
+
--- .scripts/setup/dependencies/apt_packages.sh
+++ .scripts/setup/dependencies/apt_packages.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-sudo add-apt-repository -y ppa:cpick/hub
-sudo apt-get update && sudo apt-get -y upgrade
-
-# BUILD TOOLS AND UTILS
-sudo apt-get install -y git vim curl zlib1g-dev build-essential \
+sudo apt-get install -y git neovim curl zlib1g-dev build-essential \
     libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 \
     libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties \
-    libffi-dev openjdk-8-jdk openjdk-8-doc \
+    libffi-dev openjdk-8-jdk openjdk-8-doc software-properties-common\
     libgdbm-dev libncurses5-dev automake \
     libtool bison libffi-dev libpq-dev libssl-dev \
-    cowsay hub
+    cowsay
+
+sudo add-apt-repository -y ppa:cpick/hub
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get install hub
+
+#fix internet
+sudo touch /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
