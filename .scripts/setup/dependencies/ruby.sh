cbe3c45b854703373cee83ca0eb3b597742ae71c
José Sousa <josesousa9000@gmail.com>
2017-10-17 11:15:07 +0100

    Fix ruby

--- .scripts/setup/dependencies/ruby.sh
+++ .scripts/setup/dependencies/ruby.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libff
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
-rvm install 2.4.0
-rvm use 2.4.0 --default
+rvm install ruby-2.4.1
+rvm use ruby-2.4.1 --default
 ruby -v
 
--- .zshrc
+++ .zshrc
@@ -51,3 +51,6 @@ export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
 #rvm
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
