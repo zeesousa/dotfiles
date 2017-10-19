9b97ba380a1a383b9371089785a25f852137050c
José Sousa <josesousa9000@gmail.com>
2017-10-19 14:11:42 +0100

    Back to rbenv again....

--- .scripts/setup/dependencies/ruby.sh
+++ .scripts/setup/dependencies/ruby.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 sudo apt-get install -y libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-rvm install ruby-2.4.1
-rvm use ruby-2.4.1 --default
-ruby -v
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+source ~/.zshrc
+
+rbenv install 2.4.2
+rbenv global 2.4.2
 
--- .zshrc
+++ .zshrc
@@ -48,9 +48,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 export PATH="$PATH:$HOME/.config/yarn/global/node_modules/.bin"
 
-#rvm
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
