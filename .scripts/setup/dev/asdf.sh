ec9bf49b1d74f2832e1f2868d15d2d666c6f79a0
José Sousa <josesousa9000@gmail.com>
2018-02-16 01:38:20 +0000

    Improve asdf script

--- .scripts/setup/dev/asdf.sh
+++ .scripts/setup/dev/asdf.sh
@@ -4,7 +4,11 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.4.2
 
 source ~/.zshrc
 
-sudo apt-get install automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  sudo apt-get install automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
+elif [[ "$OSTYPE" == "darwin" ]]; then
+  brew install coreutils automake autoconf openssl libyaml readline libxslt libtool unixodbc gpg
+fi
 
 asdf plugin-add ruby
 asdf plugin-add nodejs
@@ -12,3 +16,8 @@ asdf plugin-add nodejs
 export GNUPGHOME="${ASDF_DIR:-$HOME/.asdf}/keyrings/nodejs" && mkdir -p "$GNUPGHOME" && chmod 0700 "$GNUPGHOME"
 ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 
+asdf install ruby "2.4.2"
+asdf install nodejs "8.9.4"
+
+asdf global ruby "2.4.2"
+asdf global nodejs "8.9.4"
