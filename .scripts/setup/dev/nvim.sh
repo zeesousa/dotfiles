e82dcd71f59c66c81fe664227c14f53db480d0fc
José Sousa <josesousa9000@gmail.com>
2018-03-09 00:19:58 +0000

    Update install scripts

--- .scripts/setup/dev/nvim.sh
+++ .scripts/setup/dev/nvim.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [[ $(apt-cache search --names-only 'neovim' | wc -c) == 0 ]]; then 
+    sudo add-apt-repository ppa:neovim-ppa/stable
+    sudo apt-get update
+fi
+
 sudo apt-get install -y neovim
 git clone https://github.com/VundleVim/Vundle.vim ~/.config/nvim/bundle/Vundle.vim
--- .scripts/setup/dev_install.sh
+++ .scripts/setup/dev_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 ~/.scripts/setup/dev/misc.sh
+~/.scripts/setup/dev/asdf.sh
+~/.scripts/setup/dev/yarn.sh
 ~/.scripts/setup/dev/nvim.sh
-~/.scripts/setup/dev/node.sh
 ~/.scripts/setup/dev/zsh.sh
 ~/.scripts/setup/dev/psql.sh
 ~/.scripts/setup/dev/vscode_packages.sh
+~/.scripts/setup/dev/fira.sh
 ~/.scripts/setup/dev/chrome_driver.sh
