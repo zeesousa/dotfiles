df3ea79751f16b0781fe66dac52e327b89cab2f8
José Sousa <josesousa9000@gmail.com>
2018-10-03 10:22:24 +0100

    Manjaro stuff
    Add nvim to vim redirector plus dconf backup

--- .scripts/dconf-dump.sh
+++ .scripts/dconf-dump.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo dconf dump / > ~/.scripts/dconf-settings
-echo "Dconf settings dumped to ~/.scripts/dconf-settings"
+dconf dump /
--- .scripts/dconf-restore.sh
+++ .scripts/dconf-restore.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-sudo dconf load / < ~/.scripts/dconf-settings
-echo "You should really logout and login to avoid any unwanted explosions"
-
+dconf load /
